72a394d015b46c70eaee758c0ea42a9d4b171c7c
Joao Pedro Salgado <salgadojoaopedro@hotmail.com>
2018-06-27 21:37:43 -0300

    Protótipo do Servidor

--- source/demo/server.php
+++ source/demo/server.php
@@ -6,18 +6,40 @@
     <title>Servidor Demonstração</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </head>
 <body>
 
-  <div class="container"></div>
-  		<div class="form-group">
-    		<label for="exampleInputMensagem">Enviar Mensagem</label>
-    		<input type="text" class="enviarMensagem" id="enviarMensagem" placeholder="Digite sua mensagem">
-  		</div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <nav class="navbar navbar-primary bg-primary">
+    <span class="navbar-brand mb-0 h1">Demonstração - Servidor</span>
+  </nav>
+
+  <div class="container">
+     <button type="button" id="pegarMensagem" class="btn btn-primary">Enviar Mensagem</button>
+  </div>
+
+<script>
+
+     $(document).ready(function(){
+        $("#pegarMensagem").click(function(){
+           get();
+        }); 
+    });
+
+     function get(){
+       $.getJSON("get.php", function(data){
+              console.log(data);
+       });           
+
+    }
+
+</script>
+
 
 </body>
 </html>
